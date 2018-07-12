2c0fac1c022ad731e504df71ac3726672ccce790
Cristian Bauza <cristian.bauza.silva@gmail.com>
2018-07-11 21:20:20 -0300

    Add security header to swagger.

--- WebAPI/Infreaestructure/SwaggerServiceExtensions.cs
+++ WebAPI/Infreaestructure/SwaggerServiceExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
+using System.Collections.Generic;
 
 namespace WebAPI.Infreaestructure
 {
@@ -12,6 +13,12 @@ namespace WebAPI.Infreaestructure
             {
                 c.SwaggerDoc("v1.0", new Info { Title = "Main API v1.0", Version = "v1.0" });
 
+                // Swagger 2.+ support
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[] { }},
+                };
+
                 c.AddSecurityDefinition("Bearer", new ApiKeyScheme
                 {
                     Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
@@ -19,6 +26,7 @@ namespace WebAPI.Infreaestructure
                     In = "header",
                     Type = "apiKey"
                 });
+                c.AddSecurityRequirement(security);
             });
 
             return services;
@@ -30,7 +38,9 @@ namespace WebAPI.Infreaestructure
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "Versioned API v1.0");
-                //c.DocExpansion(new DocExpansion());
+
+                c.DocumentTitle = "Title Documentation";
+                c.DocExpansion(DocExpansion.None);
             });
 
             return app;
